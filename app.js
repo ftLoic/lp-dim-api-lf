1879ccd6b081b130a3a9700f57ec7b69ce84bc17
Loïc <36301183+ftLoic@users.noreply.github.com>
2022-03-08 16:26:05 +0100

    DRM

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var studentsRouter = require('./routes/students');
+
+var app = express();
+
+var dev_db_url = "mongodb+srv://ftLoic:SN6n1U5jkfebaIhS@iut.53r6r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/students', studentsRouter);
+
+module.exports = app;
